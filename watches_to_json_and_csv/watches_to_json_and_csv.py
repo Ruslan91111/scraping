c589a79b3e414e2cdf854bc56b636fbed6080efd
Ruslan9111 <ruslan_ru911@mail.ru>
2023-01-06 00:25:41 +0500

    watches_save_to_json

--- watches_to_json_and_csv/watches_to_json_and_csv.py
+++ watches_to_json_and_csv/watches_to_json_and_csv.py
@@ -1,8 +1,16 @@
+"""
+Программа для скрапинга данных и их дальнейшей записи в json и csv форматах.
+Scraping с сайта с пяти страниц пагинации.
+"""
+import csv
+import json
 import os
 import time
 
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+
 
 def get_all_pages():
     headers = {
@@ -42,7 +50,22 @@ def get_all_pages():
     # функция возвращает количество страниц
     return pages_count + 1
 
+
 def collect_data(pages_count):
+    cur_date = datetime.now().strftime("%d_%m_%y")
+
+
+    # создаем и открываем на запись файл формата .csv
+    with open(f"data_{cur_date}.csv", "w", encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            (
+                "Артикул",
+                "Ссылка",
+            )
+        )
+
+    data = []
 
     # цикл, который будет читать полученные ранее страницы
     for page in range(1, pages_count):
@@ -53,13 +76,31 @@ def collect_data(pages_count):
         items_cards = soup.find_all("a", class_="product-item__link")
 
         for item in items_cards:
-            product_article = item.find("p", class_ ="product-item__articul").text
-            product_url = item.get("href")
+            product_article = item.find("p", class_ ="product-item__articul").text.strip()
+            product_url = f"https://shop.casio.ru{item.get('href')}"
 
-            print(f"Article: {product_article} - Url: {product_url}")
+            # print(f"Article: {product_article} - Url: {product_url}")
+            data.append(
+                {"product_article": product_article,
+                 "product_url": product_url
+                 }
+            )
 
+            with open(f"data_{cur_date}.csv", "a", encoding='utf-8') as file:
+                writer = csv.writer(file)
 
+                writer.writerow(
+                    (
+                        product_article,
+                        product_url
+                    )
+                )
 
+        print(f"[INFO] Обработана страница {page}/5")
+
+    # запись в json-файл
+    with open(f"data_{cur_date}.json", "a") as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
 
 
 def main():
